77a86946d58e8d84370c119824f217bd3c86de42
Lillie Schachter <lillieschachter@gmail.com>
2022-10-13 17:31:34 -0400

    add cleaned library.py

--- library.py
+++ library.py
@@ -2,25 +2,22 @@ from book import Book
 
 
 class Library():
-	def __init__(self):
-		"""Initialize the empty book list"""
+    def __init__(self):
+        """Initialize the empty book list"""
+        pass
 
-	def add(self):
-		"""Add a Book object with the given title and author to the book list"""
-		pass
+    def add_title(self):
+        """Add a Book object with the given title and author to the book list"""
+        pass
 
-	def count_books(self):
-		"""Return the number of books currently in the booklist"""
-		pass
+    def count_books(self):
+        """Return the number of books currently in the booklist"""
+        pass
 
-	def remove_title(self):
-		"""Remove a book from the book list"""
-		pass
+    def remove_title(self):
+        """Remove a book from the book list"""
+        pass
 
-	def display_titles(self):
-		"""Print out all titles currently in the book list"""
-		pass
-
-	def is_empty(self):
-		"""Return True if the book list is empty, False if not"""
-		pass
+    def is_empty(self):
+        """Return True if the book list is empty, False if not"""
+        return self.books == []
